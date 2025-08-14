89e521092d10e01e5611d3e5862de6cba9441a83
Parker0000000 <flowers.par8470@stu.stech.edu>
2025-08-14 11:02:31 -0600

    added the battleship file

---
+++ App.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner input = new Scanner(System.in);
+
+        // P1 starting board
+        int[][] P1startboard = new int[10][10];
+
+    }
+}
