fcd43cea935336529977f9c970d4fc0f8564655f
Parker0000000 <flowers.par8470@stu.stech.edu>
2025-08-19 10:16:26 -0600

    added more to the game board currently broken

--- App.java
+++ App.java
@@ -3,9 +3,19 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
-
-        // P1 starting board
-        int[][] P1startboard = new int[10][10];
+        string ship ="x";
+        
+        int[][] P1startboard;
+        P1startboard = new int[10][10];
+        int p1ships = 0;
+        while (p1ships < 17) {
+            int x = input.nextint();
+            int y = input.nextint();
+            P1startboard[x][y] = ship;
+            system.out.println(P1startboard[x][y]);
+            system.out.println(p1ships);
+            p1ships++;
+        }
 
     }
 }
