277504d3f7f49e305c61b65a453074f97bbfd0ff
Parker0000000 <flowers.par8470@stu.stech.edu>
2025-08-26 10:17:42 -0600

    started working on adding the ships

--- App.java
+++ App.java
@@ -3,11 +3,32 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
-        int ship = 1;
-        int[][] P1startboard;
-        P1startboard = new int[10][10];
-        int[][] P2startboard;
-        P2startboard = new int[10][10];
+
+        //starting boards
+        String[][] P1startboard = {
+            {" ", " ", " ", " ", " ", " ", " ", " ", " ", " "},
+            {" ", " ", " ", " ", " ", " ", " ", " ", " ", " "},
+            {" ", " ", " ", " ", " ", " ", " ", " ", " ", " "},
+            {" ", " ", " ", " ", " ", " ", " ", " ", " ", " "},
+            {" ", " ", " ", " ", " ", " ", " ", " ", " ", " "},
+            {" ", " ", " ", " ", " ", " ", " ", " ", " ", " "},
+            {" ", " ", " ", " ", " ", " ", " ", " ", " ", " "},
+            {" ", " ", " ", " ", " ", " ", " ", " ", " ", " "},
+            {" ", " ", " ", " ", " ", " ", " ", " ", " ", " "},
+            {" ", " ", " ", " ", " ", " ", " ", " ", " ", " "}
+        };
+        String[][] P2startboard = {
+            {" ", " ", " ", " ", " ", " ", " ", " ", " ", " "},
+            {" ", " ", " ", " ", " ", " ", " ", " ", " ", " "},
+            {" ", " ", " ", " ", " ", " ", " ", " ", " ", " "},
+            {" ", " ", " ", " ", " ", " ", " ", " ", " ", " "},
+            {" ", " ", " ", " ", " ", " ", " ", " ", " ", " "},
+            {" ", " ", " ", " ", " ", " ", " ", " ", " ", " "},
+            {" ", " ", " ", " ", " ", " ", " ", " ", " ", " "},
+            {" ", " ", " ", " ", " ", " ", " ", " ", " ", " "},
+            {" ", " ", " ", " ", " ", " ", " ", " ", " ", " "},
+            {" ", " ", " ", " ", " ", " ", " ", " ", " ", " "}
+        };
 
         //player ones ships
         int p1ships = 0;
@@ -30,41 +51,36 @@ public class App {
                 num++;
             }
             System.out.println();
+            System.out.println("  |---|---|---|---|---|---|---|---|---|---|");
+
+            //P1 ships
+            int num3 = 0;
+            System.out.println("Select the direction of your aircraft carrier");
+            System.out.println("H = horizontal V = vertical");
+            String AC = input.nextLine();
+            Char AR = AC.charAt(0);
+            System.out.println("Select the back tile");
             System.out.print("Enter the x axis: ");
             int x = input.nextInt();
             System.out.print("Enter the y axis: ");
             int y = input.nextInt();
-            P1startboard[x][y] = ship;
-            p1ships++;
-        }
-        
-        //player twos ships
-        int p2ships = 0;
-        while (p2ships < 17) {
-            System.out.println("Player two");
-            
-            int num = 0;
-            System.out.print("    0   1   2   3   4   5   6   7   8   9");
-            while (num < 10) {
-                System.out.println();
-                System.out.println("  |---|---|---|---|---|---|---|---|---|---|");
-                int num2 = 0;
-                
-                System.out.print(num + " ");
-                while (num2 < 10) {
-                    System.out.print("| " + P2startboard[num][num2] + " ");
-                    num2++;
+            String V = "V";
+            if (AR == V) {
+                while (num3 < 4) {
+                    P1startboard[y][x] = "A";
+                    x++;
+                    num3++;
                 }
-                    System.out.print("|");
-                num++;
             }
-            System.out.println();
-            System.out.print("Enter the x axis: ");
-            int x = input.nextInt();
-            System.out.print("Enter the y axis: ");
-            int y = input.nextInt();
-            P2startboard[x][y] = ship;
-            p2ships++;
+            else {
+                while (num3 < 4) {
+                    P1startboard[y][x] = "A";
+                    y++;
+                    num3++;
+                }
+            }
+            
+            p1ships++;
         }
     }
 }
