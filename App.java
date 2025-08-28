6d3be669a281f9005b5a36da8b90d411eac6eaaf
Parker0000000 <flowers.par8470@stu.stech.edu>
2025-08-28 10:17:00 -0600

    got the ships somewhat working

--- App.java
+++ App.java
@@ -54,32 +54,144 @@ public class App {
             System.out.println("  |---|---|---|---|---|---|---|---|---|---|");
 
             //P1 ships
+
+            //Aircraft Carrier
             int num3 = 0;
             System.out.println("Select the direction of your aircraft carrier");
             System.out.println("H = horizontal V = vertical");
-            String AC = input.nextLine();
-            Char AR = AC.charAt(0);
+            String V = input.next();
             System.out.println("Select the back tile");
             System.out.print("Enter the x axis: ");
             int x = input.nextInt();
             System.out.print("Enter the y axis: ");
             int y = input.nextInt();
-            String V = "V";
-            if (AR == V) {
-                while (num3 < 4) {
+            if (V.equalsIgnoreCase("H")) {
+                while (num3 < 5) {
                     P1startboard[y][x] = "A";
                     x++;
                     num3++;
                 }
             }
             else {
-                while (num3 < 4) {
+                if (V.equalsIgnoreCase("V")){
+                while (num3 < 5) {
                     P1startboard[y][x] = "A";
                     y++;
                     num3++;
+                    }
                 }
             }
-            
+
+
+            //Battleship
+            num3 = 0;
+            System.out.println("Select the direction of your battleship");
+            System.out.println("H = horizontal V = vertical");
+            V = input.next();
+            System.out.println("Select the back tile");
+            System.out.print("Enter the x axis: ");
+            x = input.nextInt();
+            System.out.print("Enter the y axis: ");
+            y = input.nextInt();
+            if (V.equalsIgnoreCase("H")) {
+                while (num3 < 4) {
+                    P1startboard[y][x] = "B";
+                    x++;
+                    num3++;
+                }
+            }
+            else {
+                if (V.equalsIgnoreCase("V")){
+                while (num3 < 4) {
+                    P1startboard[y][x] = "B";
+                    y++;
+                    num3++;
+                    }
+                }
+            }
+
+            //Cruiser
+            num3 = 0;
+            System.out.println("Select the direction of your cruiser");
+            System.out.println("H = horizontal V = vertical");
+            V = input.next();
+            System.out.println("Select the back tile");
+            System.out.print("Enter the x axis: ");
+            x = input.nextInt();
+            System.out.print("Enter the y axis: ");
+            y = input.nextInt();
+            if (V.equalsIgnoreCase("H")) {
+                while (num3 < 3) {
+                    P1startboard[y][x] = "C";
+                    x++;
+                    num3++;
+                }
+            }
+            else {
+                if (V.equalsIgnoreCase("V")){
+                while (num3 < 3) {
+                    P1startboard[y][x] = "C";
+                    y++;
+                    num3++;
+                    }
+                }
+            }
+
+            //Submarine
+            num3 = 0;
+            System.out.println("Select the direction of your submarine");
+            System.out.println("H = horizontal V = vertical");
+            V = input.next();
+            System.out.println("Select the back tile");
+            System.out.print("Enter the x axis: ");
+            x = input.nextInt();
+            System.out.print("Enter the y axis: ");
+            y = input.nextInt();
+            if (V.equalsIgnoreCase("H")) {
+                while (num3 < 3) {
+                    P1startboard[y][x] = "S";
+                    x++;
+                    num3++;
+                }
+            }
+            else {
+                if (V.equalsIgnoreCase("V")){
+                while (num3 < 3) {
+                    P1startboard[y][x] = "S";
+                    y++;
+                    num3++;
+                    }
+                }
+            }
+
+            //Destroyer
+            num3 = 0;
+            System.out.println("Select the direction of your destroyer");
+            System.out.println("H = horizontal V = vertical");
+            V = input.next();
+            System.out.println("Select the back tile");
+            System.out.print("Enter the x axis: ");
+            x = input.nextInt();
+            System.out.print("Enter the y axis: ");
+            y = input.nextInt();
+            if (V.equalsIgnoreCase("H")) {
+                while (num3 < 2) {
+                    P1startboard[y][x] = "D";
+                    x++;
+                    num3++;
+                }
+            }
+            else {
+                if (V.equalsIgnoreCase("V")){
+                while (num3 < 2) {
+                    P1startboard[y][x] = "D";
+                    y++;
+                    num3++;
+                    }
+                }
+            }
+
+
             p1ships++;
         }
     }
